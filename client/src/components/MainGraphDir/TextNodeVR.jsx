0a5c699f56889db979a7a57d4dcf16528f01d4d0
aron luk <aron_luk@a.com>
2025-07-02 16:44:56 +0800

    Update TextNodeVR.jsx

--- client/src/components/MainGraphDir/TextNodeVR.jsx
+++ client/src/components/MainGraphDir/TextNodeVR.jsx
@@ -2,26 +2,14 @@ import {ForceGraphVR} from 'react-force-graph';
 
 import React from 'react';
 
-
-import cloneDeep from 'lodash/cloneDeep';
-
-// let SpriteText=window.SpriteText
-// import SpriteText from "//unpkg.com/three-spritetext/dist/three-spritetext.mjs";
 import SpriteText from 'three-spritetext';
 import {l} from "../../firebase/firebase";
 
 export default function TextNodeVR({dd,...props}) {
-    const [text, setText] = React.useState("hello world");
-    l("text333333333333333", text)
     let c= props.c;
-
-
-    // const newdd = cloneDeep(dd);   If you do deep clone it seems the graph will render For state change, Because the engine never got to modify the original state
     const newdd = (dd);
 
     return <>
-        <button onClick={() => {setText(d=> d+'1')}}>Change Text</button>
-        <p>Use W-A-S-D to move,    drag mouse to change view angle</p>
         <ForceGraphVR
         graphData={newdd}
         nodeAutoColorBy="group"
@@ -50,10 +38,6 @@ export default function TextNodeVR({dd,...props}) {
             };
 
             let label = replaceStringAinB("everythingallaccount", label2)
-
-
-
-
 
             // if there is a name, we use the name, if there is no, we use the ID.
             const sprite = new SpriteText(label);
