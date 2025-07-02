f2224c2fda00351fc2cd9ee04d901017975f1703
aron luk <aron_luk@a.com>
2025-07-02 13:31:20 +0800

    a

--- client/src/components/minorsss/Dashboard2.jsx
+++ client/src/components/minorsss/Dashboard2.jsx
@@ -1,17 +1,12 @@
 import React, {Component, useEffect, useState} from 'react';
-import buttonList from './initialButtonList';
-import Layout from '../../containers/Layout';
-import {auth} from '../../firebase';
 import './Dashboard.css';
 import Home from "../Home";
-import {cgg} from "../../util/helperfile";
 
 const Dashboard = ({ providerData }) => {
 
     return (
-        <Layout>
+
             <Home />
-        </Layout>
     );
 };
 
