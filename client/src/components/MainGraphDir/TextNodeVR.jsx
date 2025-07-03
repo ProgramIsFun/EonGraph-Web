8c8eeb484a38a5c9fdbfee1dd0da821d4b602d13
aron luk <aron_luk@a.com>
2025-07-03 10:11:03 +0800

    Update TextNodeVR.jsx

--- client/src/components/MainGraphDir/TextNodeVR.jsx
+++ client/src/components/MainGraphDir/TextNodeVR.jsx
@@ -7,61 +7,58 @@ import SpriteText from 'three-spritetext';
 import {l} from "../../util/log11";
 
 function TextNodeVR(props) {
-    let dd= props.dd;
-    let c= props.c;
+    let dd = props.dd;
+    let c = props.c;
     const newdd = (dd);
 
     return <>
         <ForceGraphVR
-        graphData={newdd}
-        nodeAutoColorBy="group"
+            graphData={newdd}
+            nodeAutoColorBy="group"
 
-        onNodeClick={(node, event) => {
-            l("Node clicked22222:", node);
-            l("Event333333333:", event);
-        }}
-
-
+            onNodeClick={(node, event) => {
+                l("Node clicked22222:", node);
+                l("Event333333333:", event);
+            }}
 
 
-
-        nodeThreeObject={node => {
-
-
-            const label2 = !node.name ? node.id : node.name;
-
-            const replaceStringAinB = (strA, strB) => {
-                // Check if stringB starts with stringA
-                if (strB.startsWith(strA)) {
-                    // Replace stringA with '1' in stringB
-                    return strB.replace(strA, 'e');
-                }
-                return strB; // No change if stringB does not start with stringA
-            };
-
-            let label = replaceStringAinB("everythingallaccount", label2)
-
-            // if there is a name, we use the name, if there is no, we use the ID.
-            const sprite = new SpriteText(label);
-            sprite.color = node.color;
-            sprite.textHeight = 8;
-            return sprite;
-        }}
+            nodeThreeObject={node => {
 
 
-        backgroundColor={"#000000"}
+                const label2 = !node.name ? node.id : node.name;
 
-        // backgroundColor="rgba(0,0,0,0)"
+                const replaceStringAinB = (strA, strB) => {
+                    // Check if stringB starts with stringA
+                    if (strB.startsWith(strA)) {
+                        // Replace stringA with '1' in stringB
+                        return strB.replace(strA, 'e');
+                    }
+                    return strB; // No change if stringB does not start with stringA
+                };
+
+                let label = replaceStringAinB("everythingallaccount", label2)
+
+                // if there is a name, we use the name, if there is no, we use the ID.
+                const sprite = new SpriteText(label);
+                sprite.color = node.color;
+                sprite.textHeight = 8;
+                return sprite;
+            }}
+
+
+            backgroundColor={"#000000"}
+
+            // backgroundColor="rgba(0,0,0,0)"
             // nodeThreeObjectExtend={true}
 
 
-        linkWidth={c.linkWidth}
-        linkDirectionalParticles={c.linkDirectionalParticles}
-        linkDirectionalParticleWidth={c.linkDirectionalParticleWidth}
-        linkDirectionalParticleSpeed={c.linkDirectionalParticleSpeed}
+            linkWidth={c.linkWidth}
+            linkDirectionalParticles={c.linkDirectionalParticles}
+            linkDirectionalParticleWidth={c.linkDirectionalParticleWidth}
+            linkDirectionalParticleSpeed={c.linkDirectionalParticleSpeed}
 
         /></>
 }
 
 
-export default connect( state => state)(TextNodeVR);
+export default connect(state => state)(TextNodeVR);
