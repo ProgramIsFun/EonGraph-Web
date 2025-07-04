13a4ece628e51edd72c6fcc80663708146dff881
aron luk <aron_luk@a.com>
2025-07-04 14:07:46 +0800

    A

--- client/src/reducers/all33.js
+++ client/src/reducers/all33.js
@@ -7,12 +7,13 @@ import {
 export const cccccccccc = {
 
   // 2D
+  "global_scale_adjustment_coefficient":
+      {"value": 0.5, "min": 0.1, "max": 1, "step": 0.1},
   "repulsive_Force_Scale":
       {"value": -100, "min": -1000, "max": 1, "step": 1},
   "node_font_size":
       {"value": 6, "min": 1, "max": 20, "step": 1},
-  "global_scale_adjustment_coefficient":
-      {"value": 0.5, "min": 0.1, "max": 1, "step": 0.1},
+
   "link_opacity":
       {"value": 0.6, "min": 0, "max": 1, "step": 0.1},
   "linkWidth":
@@ -25,6 +26,10 @@ export const cccccccccc = {
       {"value": 0.002, "min": 0.001, "max": 0.01, "step": 0.001},
 
   // VR
+    "VR_global_scale_adjustment_coefficient":
+        {"value": 0.5, "min": 0.1, "max": 1, "step": 0.1},
+    "VR_repulsive_Force_Scale":
+            {"value": -100, "min": -1000, "max": 1, "step": 1},
   "VR_node_font_size":
         {"value": 6, "min": 1, "max": 20, "step": 1},
   "VR_linkOpacity":
@@ -55,7 +60,8 @@ const initialState = {
     "linkDirectionalParticleSpeed": 0.002,
 
     // VR
-
+    "VR_global_scale_adjustment_coefficient": 0.5,
+    "VR_repulsive_Force_Scale": -100,
     "VR_node_font_size": 6,
     "VR_linkOpacity": 0.6,
     "VR_linkWidth":0.6,
