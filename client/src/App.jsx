d1797cf2189ba16cad6c2119b5d670e32bbe1331
a <a@a.com>
2025-07-05 22:08:08 +0800

    a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
-import Dashboard2 from "./components/Dashboard2";
+import Dashboard2 from "./components/Dashboard";
 import Alert from './components/widgets/Alert';
 
 function App() {
