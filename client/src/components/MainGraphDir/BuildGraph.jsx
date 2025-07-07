3bff100973c8a95b739b3b4f5dd8bf48a2f2d975
a <a@a.com>
2025-07-07 22:34:33 +0800

    a

--- client/src/components/MainGraphDir/BuildGraph.jsx
+++ client/src/components/MainGraphDir/BuildGraph.jsx
@@ -43,7 +43,7 @@ function BuildGraph(props) {
         if (!(b || node.collapsed)) {
             // l("node is not objectToBeInspected or collapsed, skipping paintRing", node.id, "objectToBeInspected", objectToBeInspected.id)
         } else {
-            l("trigger")
+
             const NODE_R = 8;
 
             ctx.beginPath();
@@ -171,54 +171,55 @@ function BuildGraph(props) {
                     l("clicked on the same node, unsetting objectToBeInspected")
                     dispatch({type: SET_OBJECT_TO_BE_INSPECTED, payload: {}})
                 } else {
+                    l("clicked on a different node")
+                    let enableLinking = true
+                    if (enableLinking) {
+                        let shouldAddLink = false;
 
+                        // check if any link in all the links have the same source and target.
+                        let some = lc.some(
+                            link => {
+                                let lll = (
+                                    link.source.id === objectToBeInspected.id
+                                    && link.target.id === node.id
+                                ) || (
+                                    link.source.id === node.id &&
+                                    link.target.id === objectToBeInspected.id
+                                )
+                                if (lll) {
+                                    l("link already exists!!!!!!!!!", link.source.id, objectToBeInspected.id, link.target.id, node.id)
+                                } else {
 
-                    let shouldAddLink = false;
-
-                    // check if any link in all the links have the same source and target.
-                    let some = lc.some(
-                        link => {
-                            let lll = (
-                                link.source.id === objectToBeInspected.id
-                                && link.target.id === node.id
-                            ) || (
-                                link.source.id === node.id &&
-                                link.target.id === objectToBeInspected.id
-                            )
-                            if (lll) {
-                                l("link already exists!!!!!!!!!", link.source.id, objectToBeInspected.id, link.target.id, node.id)
-                            } else {
-
+                                }
+                                return lll
                             }
-                            return lll
+                        );
+                        shouldAddLink = !some
+                        l("shouldAddLink", shouldAddLink, "objectToBeInspected.id", objectToBeInspected.id, "node.id", node.id)
+                        if (shouldAddLink) {
+                            let linkId = uuidv4()
+                            l("objectToBeInspected.id", objectToBeInspected.id, "node.id", node.id)
+                            const interimLink = {
+                                id: linkId,
+                                source: objectToBeInspected.id,
+                                target: node.id,
+                                name: 'link_' + linkId
+                            };
+                            // setdd(prevNc => ({
+                            //     ...prevNc, // Spread to copy other properties of nc, if there are any
+                            //     links: [...prevNc.links, interimLink], // Create a new array with all old nodes plus the new one
+                            // }));
+                            let payload = [
+                                ...dd.links, interimLink
+                            ];
+                            l("payload3333333333333333", payload)
+                            dispatch({
+                                type: CHANGE_LINKS, payload: payload
+                            })
+                            dispatch({type: SET_OBJECT_TO_BE_INSPECTED, payload: {}})
                         }
-                    );
-                    shouldAddLink = !some
-                    if (shouldAddLink) {
-                        let linkId = uuidv4()
-                        l("objectToBeInspected.id", objectToBeInspected.id, "node.id", node.id)
-                        const interimLink = {
-                            id: linkId,
-                            source: objectToBeInspected.id,
-                            target: node.id,
-                            name: 'link_' + linkId
-                        };
-                        // setdd(prevNc => ({
-                        //     ...prevNc, // Spread to copy other properties of nc, if there are any
-                        //     links: [...prevNc.links, interimLink], // Create a new array with all old nodes plus the new one
-                        // }));
-                        dispatch({
-                            type: CHANGE_LINKS, payload: [
-                                    ...dd.links, interimLink
-
-                            ]
-                        })
-
-
-                        dispatch({type: SET_OBJECT_TO_BE_INSPECTED, payload: {}})
+                    } else {
                     }
-
-
                 }
             }
 
