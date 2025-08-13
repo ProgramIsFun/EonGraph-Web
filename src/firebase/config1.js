4a3ea7e5363a940d9e131aac84044fcaddaf174a
aron luk <aron_luk@a.com>
2025-08-13 18:39:17 +0800

    a

---
+++ src/firebase/config1.js
@@ -0,0 +1,8 @@
+export const  firebaseConfig = {
+    apiKey: "AIzaSyAKXgCEIiHEUitw3Dt8DifFWZ-6l7xeeYQ",
+    authDomain: "githubvisssss202324.firebaseapp.com",
+    projectId: "githubvisssss202324",
+    storageBucket: "githubvisssss202324.appspot.com",
+    messagingSenderId: "342402020319",
+    appId: "1:342402020319:web:d9ee351cb10f517d634a6a"
+}
