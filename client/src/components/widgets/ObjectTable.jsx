abe048a637be67e42f58b70243510da783aa4417
username71 <your.email@example.com>
2025-11-06 15:14:29 +0800

    a

--- client/src/components/widgets/ObjectTable.jsx
+++ client/src/components/widgets/ObjectTable.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import ReactJson from '@microlink/react-json-view'
 import JsonView from '@uiw/react-json-view';
 
-import {removegithubRepoNode, rename} from "../../autil/helperfile";
+import {aaaaaaa, removegithubRepoNode, rename} from "../../autil/helperfile";
 import {l} from "../../autil/log11";
 import {connect, useDispatch} from "react-redux";
 
@@ -63,37 +63,10 @@ const ObjectTable = (props) => {
     };
 
 
-    // check if object.reponested.html_url is not undefined, if yes, we get the link
-    const link = props.objectToBeInspected.reponested && props.objectToBeInspected.reponested.html_url;
-    let aaaaaaa = {
-        "--w-rjv-color": "#9cdcfe",
-        "--w-rjv-key-number": "#268bd2",
-        "--w-rjv-key-string": "#9cdcfe",
-        "--w-rjv-background-color": "#000000ff",
-        "--w-rjv-line-color": "#36334280",
-        "--w-rjv-arrow-color": "#838383",
-        "--w-rjv-edit-color": "#9cdcfe",
-        "--w-rjv-info-color": "#9c9c9c7a",
-        "--w-rjv-update-color": "#9cdcfe",
-        "--w-rjv-copied-color": "#9cdcfe",
-        "--w-rjv-copied-success-color": "#28a745",
-        "--w-rjv-curlybraces-color": "#d4d4d4",
-        "--w-rjv-colon-color": "#d4d4d4",
-        "--w-rjv-brackets-color": "#d4d4d4",
-        "--w-rjv-ellipsis-color": "#cb4b16",
-        "--w-rjv-quotes-color": "#9cdcfe",
-        "--w-rjv-quotes-string-color": "#ce9178",
-        "--w-rjv-type-string-color": "#ce9178",
-        "--w-rjv-type-int-color": "#b5cea8",
-        "--w-rjv-type-float-color": "#b5cea8",
-        "--w-rjv-type-bigint-color": "#b5cea8",
-        "--w-rjv-type-boolean-color": "#569cd6",
-        "--w-rjv-type-date-color": "#b5cea8",
-        "--w-rjv-type-url-color": "#3b89cf",
-        "--w-rjv-type-null-color": "#569cd6",
-        "--w-rjv-type-nan-color": "#859900",
-        "--w-rjv-type-undefined-color": "#569cd6"
-    }
+    // isRepo
+    const link = props.objectToBeInspected.reponested
+        && props.objectToBeInspected.reponested.html_url;
+
     let aaaaaaa1 = async () => {
         await removegithubRepoNode(objectToBeInspected, dd, setdd, dispatch);
     }
@@ -165,8 +138,11 @@ const ObjectTable = (props) => {
                         </button>
 
 
-                        {link && <a href={link} target="_blank" rel="noreferrer">
-                            View on GitHub</a>
+                        {
+                            link &&
+                            <a href={link} target="_blank" rel="noreferrer">
+                            View on GitHub
+                            </a>
                         }
                         {
                             element
