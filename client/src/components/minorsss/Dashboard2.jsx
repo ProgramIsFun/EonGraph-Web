655c0b7649582d022e46e788107421bdfa7e7245
username71 <your.email@example.com>
2025-11-06 15:13:01 +0800

    Update Dashboard2.jsx

--- client/src/components/minorsss/Dashboard2.jsx
+++ client/src/components/minorsss/Dashboard2.jsx
@@ -7,37 +7,7 @@ import Home from "../Home";
 import {cgg} from "../../util/helperfile";
 
 const Dashboard = ({ providerData }) => {
-
-
-    // please note that this is originally a class component
-    // but I remove and reform it to a functional component in a git commit. 9d7967697c237eb7f7635c79f72782033934e47b
-
-
-
-
-    const [count, setCount] = useState(0);
-
-
-    const [buttonListState, setButtonListState] = useState(buttonList);
-    const [providerDataState, setProviderDataState] = useState(providerData);
-
-    useEffect(() => {
- }, [providerDataState]); // Only re-run the effect if providerDataState changes
-
-  
-
-
-    const updateButtonList = (buttonList, providerName, visible) => {
-        // cgg(providerName, "visible -> false");
-        return ({
-            ...buttonList,
-            [providerName]: {
-                ...buttonList[providerName],
-                visible
-            }
-        });
-    };
-
+    
     return (
         <Layout>
             <Home />
