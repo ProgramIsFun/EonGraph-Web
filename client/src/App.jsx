b2ad02c3fece0f55e93cbee61097a698c09464e4
username71 <your.email@example.com>
2025-11-06 15:13:15 +0800

    a

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Dashboard2 from "./components/minorsss/Dashboard2";
-import { AAAA1 } from "./components/AAAA1";
 import Alert from './layout/Alert';
 
 function App() {
@@ -11,8 +10,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/dashboard" element={<Dashboard2 />} />
-                    <Route path="/1" element={<AAAA1 />} />
-                    <Route path="/" element={<Navigate to="/dashboard" replace />} />
+                   <Route path="/" element={<Navigate to="/dashboard" replace />} />
                 </Routes>
             </BrowserRouter>
         </>
