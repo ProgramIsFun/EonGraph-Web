9f0b35a2b2f5eb7836a752f67d5a58ca238bea2f
username71 <your.email@example.com>
2025-11-06 15:12:55 +0800

    a

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM1 from 'react-dom';
 import ReactDOM from 'react-dom/client';
 
-// import { Neo4jProvider, createDriver } from 'use-neo4j'
-
 
 
 
@@ -14,18 +12,11 @@ import App from './App';
 import Wwwwwwwwww from "./ttttttes/Wwwwwwwwww";
 import Wwwwwwwwww2 from "./ttttttes/Wwwwwwwwww2";
 import Wwwwwwwwww3 from "./ttttttes/Wwwwwwwwww3";
-// import Wwwwwwwwww4 from "./ttttttes/Wwwwwwwwww4";
-
-//
-// const driver = createDriver('neo4j',
-//     'neo4j+ssc://7df0.databases.neo4j.io',
-//     7687, 'neo4j',
-//     'B0Qqy')
 
 
 
 let aaaaaaaappppppppp=<React.StrictMode>
-    { 0?
+    { 1 ?
 
 
         [
@@ -42,19 +33,12 @@ let aaaaaaaappppppppp=<React.StrictMode>
             // <Wwwwwwwwww4></Wwwwwwwwww4>,
 
 
-        ][3]
+        ][0]
 
         :
 
 
-
-
-        // <Neo4jProvider driver={driver}>
-
-
-            <App/>
-        // </Neo4jProvider>
-
+        <App/>
 
 
 
