9bd53493e43de8948a8ccb3ea704092cb6c3a9a6
username71 <your.email@example.com>
2025-11-06 15:15:15 +0800

    A

--- src/autil/helperfile.js
+++ src/autil/helperfile.js
@@ -725,5 +725,6 @@ export const replaceStringAinB = (strA, strB) => {
 };
 
 
-
-
+export const distance1and2 = (node1, node2) => {
+    return Math.sqrt(Math.pow(node1.x - node2.x, 2) + Math.pow(node1.y - node2.y, 2));
+};
