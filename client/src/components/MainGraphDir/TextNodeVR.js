1bff7aae5dd91691645ceb481ced174fbe3ef0a7
username71 <your.email@example.com>
2025-11-06 15:12:43 +0800

    a

--- client/src/components/MainGraphDir/TextNodeVR.js
+++ client/src/components/MainGraphDir/TextNodeVR.js
@@ -9,11 +9,14 @@ import cloneDeep from 'lodash/cloneDeep';
 // import SpriteText from "//unpkg.com/three-spritetext/dist/three-spritetext.mjs";
 import SpriteText from 'three-spritetext';
 
-export default function TextNodeVR({dd}) {
+export default function TextNodeVR({dd,...props}) {
     // console.log(dd)
     console.log("spriteTextttttttttttttttttttttttt", SpriteText)
     console.log("enteringVR, deepclone property,await mutation to original prop")
     
+    let c= props.c;
+
+
     const newdd = cloneDeep(dd);
     return <>
         <p>Use W-A-S-D to move,    drag mouse to change view angle</p>
@@ -21,14 +24,24 @@ export default function TextNodeVR({dd}) {
         graphData={newdd}
         nodeAutoColorBy="group"
         nodeThreeObject={node => {
-
-
             // if there is a name, we use the name, if there is no, we use the ID.
             const sprite = new SpriteText(node.name || node.id);
             sprite.color = node.color;
             sprite.textHeight = 8;
             return sprite;
         }}
+
+
+
+
+
         // nodeThreeObjectExtend={true}
-    /></>
+
+
+        linkWidth={c.linkWidth}
+        linkDirectionalParticles={c.linkDirectionalParticles}
+        linkDirectionalParticleWidth={c.linkDirectionalParticleWidth}
+        linkDirectionalParticleSpeed={c.linkDirectionalParticleSpeed}
+
+        /></>
 }
