f7c2f0c36366b7cd837813858c4364a1bc2aebbe
username71 <your.email@example.com>
2025-11-06 15:13:07 +0800

    a

---
+++ client/src/reducers/all33.js
@@ -0,0 +1,75 @@
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
+  NO_REPOS
+} from '../actions/types';
+
+const initialState = {
+  settings:{
+    "repulsive_Force_Scale": -100,
+    "node_font_size": 6,
+    "global_scale_adjustment_coefficient": 0.5,
+    "link_opacity": 0.6,
+    "linkWidth": 0.3,
+    "linkDirectionalParticles": 4,
+    "linkDirectionalParticleWidth": 2,
+    "linkDirectionalParticleSpeed": 0.002
+  },
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {}
+};
+
+function all33Reducer(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_PROFILE:
+    case UPDATE_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false
+      };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
+    case PROFILE_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+        profile: null
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: []
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
+      };
+    case NO_REPOS:
+      return {
+        ...state,
+        repos: []
+      };
+    default:
+      return state;
+  }
+}
+
+export default all33Reducer;
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,11 @@ import alert from './alert';
 import auth from './auth';
 import profile from './profile';
 import post from './post';
-
+import all33 from './all33';
 export default combineReducers({
   alert,
   auth,
   profile,
-  post
+  post,
+  all33
 });
