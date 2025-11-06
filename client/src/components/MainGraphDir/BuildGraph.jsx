9db34661c965eb0991f5b44c87f32de1d6f4a073
username71 <your.email@example.com>
2025-11-06 15:13:20 +0800

    Update BuildGraph.jsx

--- client/src/components/MainGraphDir/BuildGraph.jsx
+++ client/src/components/MainGraphDir/BuildGraph.jsx
@@ -8,7 +8,8 @@ import {connect} from "react-redux";
 
 function BuildGraph(props) {
 
-    let c = props.c
+    // let c = props.c
+    let c= props.all33.settings
 
     let setdd= props.setdd
     let repo= props.repo
